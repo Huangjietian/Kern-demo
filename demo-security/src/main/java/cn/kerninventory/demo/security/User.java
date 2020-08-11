baf84037fbd91fa263c5a2c731a19c7d0a271ce4
Kern <825761175@qq.com>
2020-08-11 16:52:22 +0800

    security

---
+++ demo-security/src/main/java/cn/kerninventory/demo/security/User.java
@@ -0,0 +1,16 @@
+package cn.kerninventory.demo.security;
+
+/**
+ * <h1>中文注释</h1>
+ * <p>
+ *     用户的身份
+ * </p>
+ *
+ * @author Kern
+ * @version 1.0
+ */
+public interface User {
+
+    int getLegality();
+
+}
---
+++ demo-security/src/main/java/cn/kerninventory/demo/security/UserAuthority.java
@@ -0,0 +1,16 @@
+package cn.kerninventory.demo.security;
+
+/**
+ * <h1>中文注释</h1>
+ * <p>
+ *     用户权限
+ * </p>
+ *
+ * @author Kern
+ * @version 1.0
+ */
+public interface UserAuthority {
+
+
+
+}
